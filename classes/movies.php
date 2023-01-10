2cfd858bdd0b6b1d7e17c2cc0acdf18143c39022
skiotto93 <fabio.forenza@hotmail.com>
2023-01-10 13:19:20 +0100

    aggiunte correzioni

---
+++ classes/movies.php
@@ -0,0 +1,55 @@
+<?php 
+
+class Movies {
+    public $title;
+    public $genres;
+    public $releaseDate;
+    public $description;
+
+    public function __construct( string $title, array $genres, int $releaseDate, string $description = "")
+    {
+        $this->title = $title;
+        $this->genres = $genres;
+        $this->releaseDate = $releaseDate;
+        $this->description = $description;
+    }
+
+    public function getInfo() {
+        return "Titolo: $this->title anno di uscita: $this->releaseDate";
+    }
+}
+
+// $the_lord_of_the_rings_I = new Movies();
+
+// $the_lord_of_the_rings_I->title = "Il Signore degli Anelli";
+// $the_lord_of_the_rings_I->genre = "Fantasy";
+// $the_lord_of_the_rings_I->releaseDate = "2001";
+// $the_lord_of_the_rings_I->description = "Dall'idilliaca terra degli hobbit agli abissi fumosi di Mordor, Frodo Baggins intraprende la sua epica ricerca per distruggere l'anello di Sauron.";
+
+// $the_batman = new Movies();
+
+// $the_batman->title = "The Batman";
+// $the_batman->genre = "Avventura";
+// $the_batman->releaseDate = "2022";
+// $the_batman->description = "Gotham City è in mano alla corruzione, che dilaga tra le più alte cariche della città, dove Batman sembra esser l'unico che cerchi di garantire giustizia. È per questo che Bruce Wayne deve essere cauto e non fidarsi facilmente, ma dalla sua parte ha sempre il fidatissimo Alfred (Andy Serkis) e può contare anche sul tenente James Gordon (Jeffrey Wright).";
+
+// $the_silence_of_the_lambs = new Movies();
+
+// $the_silence_of_the_lambs->title = "Il Silenzio degli Innocenti";
+// $the_silence_of_the_lambs->genre = "Thriller";
+// $the_silence_of_the_lambs->releaseDate = "1991";
+// $the_silence_of_the_lambs->description = "Hannibal Lecter è uno psichiatra geniale, un uomo colto e raffinato. Ed è anche un pericoloso psicopatico e un feroce assassino. Ma è l'unica risorsa che ha a disposizione la giovane agente dell'FBI Clarice Starling per rintracciare Buffalo Bill, l'imprendibile \"mostro\" che scuoia le sue vittime.";
+
+// $nightmare = new Movies();
+
+// $nightmare->title = "Nightmare - Dal profondo della notte";
+// $nightmare->genre = "Horror";
+// $nightmare->releaseDate = "1984";
+// $nightmare->description = "Tina Grey ha un incubo dove si ritrova in una decrepita caldaia e viene inseguita da una misteriosa figura dalla pelle bruciata che prova ad attaccarla con il suo guanto artigliato.";
+
+// var_dump($the_lord_of_the_rings_I);
+// var_dump($the_batman);
+// var_dump($the_silence_of_the_lambs);
+// var_dump($nightmare);
+
+?>
--- index.php
+++ index.php
@@ -1,44 +1,42 @@
 <?php 
+require_once __DIR__ . "/classes/movies.php";
 
-class Movies {
-    public $title;
-    public $genre;
-    public $releaseDate;
-    public $description;
-}
+$movies = [
+    //Gli argomenti di una class (Movies[description]) opzionali vanno inseriti alla fine
+    new Movies("Il Signore degli Anelli", ["Fantasy", "Avventura"], 2001, "Dall'idilliaca terra degli hobbit agli abissi fumosi di Mordor, Frodo Baggins intraprende la sua epica ricerca per distruggere l'anello di Sauron."),
+    new Movies("The Batman", ["Avventura", "Azione"], 2022),
+    new Movies("Il Silenzio degli Innocenti", ["Thriller","Giallo"], 1991, "Hannibal Lecter è uno psichiatra geniale, un uomo colto e raffinato. Ed è anche un pericoloso psicopatico e un feroce assassino. Ma è l'unica risorsa che ha a disposizione la giovane agente dell'FBI Clarice Starling per rintracciare Buffalo Bill, l'imprendibile \"mostro\" che scuoia le sue vittime."),
+    new Movies("Nightmare - Dal profondo della notte", ["Horror", "Mystery"], 1984)
+];
 
-$the_lord_of_the_rings_I = new Movies();
-
-$the_lord_of_the_rings_I->title = "Il Signore degli Anelli";
-$the_lord_of_the_rings_I->genre = "Fantasy";
-$the_lord_of_the_rings_I->releaseDate = "2001";
-$the_lord_of_the_rings_I->description = "Dall'idilliaca terra degli hobbit agli abissi fumosi di Mordor, Frodo Baggins intraprende la sua epica ricerca per distruggere l'anello di Sauron.";
-
-$the_batman = new Movies();
-
-$the_batman->title = "The Batman";
-$the_batman->genre = "Avventura";
-$the_batman->releaseDate = "2022";
-$the_batman->description = "Gotham City è in mano alla corruzione, che dilaga tra le più alte cariche della città, dove Batman sembra esser l'unico che cerchi di garantire giustizia. È per questo che Bruce Wayne deve essere cauto e non fidarsi facilmente, ma dalla sua parte ha sempre il fidatissimo Alfred (Andy Serkis) e può contare anche sul tenente James Gordon (Jeffrey Wright).";
-
-$the_silence_of_the_lambs = new Movies();
-
-$the_silence_of_the_lambs->title = "Il Silenzio degli Innocenti";
-$the_silence_of_the_lambs->genre = "Thriller";
-$the_silence_of_the_lambs->releaseDate = "1991";
-$the_silence_of_the_lambs->description = "Hannibal Lecter è uno psichiatra geniale, un uomo colto e raffinato. Ed è anche un pericoloso psicopatico e un feroce assassino. Ma è l'unica risorsa che ha a disposizione la giovane agente dell'FBI Clarice Starling per rintracciare Buffalo Bill, l'imprendibile \"mostro\" che scuoia le sue vittime.";
-
-$nightmare = new Movies();
-
-$nightmare->title = "Nightmare - Dal profondo della notte";
-$nightmare->genre = "Horror";
-$nightmare->releaseDate = "1984";
-$nightmare->description = "Tina Grey ha un incubo dove si ritrova in una decrepita caldaia e viene inseguita da una misteriosa figura dalla pelle bruciata che prova ad attaccarla con il suo guanto artigliato.";
-
-var_dump($the_lord_of_the_rings_I);
-var_dump($the_batman);
-var_dump($the_silence_of_the_lambs);
-var_dump($nightmare);
-
-
+var_dump($movies)
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP Movies</title>
+</head>
+<body>
+    <h1>Lista film:</h1>
+    <ul>
+        <?php foreach( $movies as $movie) { ?>
+        <li>
+            <h3><?php echo $movie->title ?></h3>
+            <span>Anno di uscita: <?php echo $movie->releaseDate ?></span>
+            <ul>
+            <?php foreach( $movie->genres as $genre) { ?>
+                <li>
+                    <?php echo ucfirst($genre) ?>
+                </li>
+            <?php } ?>
+            </ul>
+            <p><?php echo $movie->description ?></p>
+        </li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
